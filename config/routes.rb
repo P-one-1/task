929ca9926c454bbae4b20907af62de9b0188b6a8
ro-pawanbharti <ror-trainee@mobiloitte.com>
2020-03-06 17:00:54 +0530

    removing to resolve

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root 'upload_user_story#index'
-  post "upload_user_story/upload"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
